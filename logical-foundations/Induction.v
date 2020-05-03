9966840e5ec6167eca498fc1945a3959ad64d055
Chris Nevers <christophernevers96@gmail.com>
2020-05-03 11:40:39 -0400

    Some induction exercises

--- logical-foundations/Induction.v
+++ logical-foundations/Induction.v
@@ -34,3 +34,45 @@ Proof.
   - simpl. rewrite -> IHn'. reflexivity.
 Qed.
 
+Theorem plus_comm : forall n m : nat,
+  n + m = m + n.
+Proof.
+  intros n m.
+  induction n as [| n' IHn'].
+  - simpl. rewrite <-plus_n_O. reflexivity.
+  - rewrite <- plus_n_Sm. rewrite <- IHn'. reflexivity.
+Qed.
+
+Theorem plus_assoc : forall n m p : nat,
+  n + (m + p) = (n + m) + p.
+Proof.
+  intros n m p.
+  induction n as [|n' IHn'].
+  - rewrite plus_O_n. simpl. reflexivity.
+  - simpl. rewrite IHn'. reflexivity.
+Qed.
+
+Fixpoint double n :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n.
+Proof.
+  induction n as [|n' IHn'].
+  - reflexivity.
+  - simpl. rewrite IHn'. rewrite <- plus_n_Sm. reflexivity.
+Qed.
+
+Theorem evenb_S : forall n : nat,
+  evenb (S n) = negb (evenb n).
+Proof.
+  induction n as [|n' IHn'].
+  - reflexivity.
+  - rewrite -> IHn'. rewrite -> negation_fn_applied_twice.
+    + simpl. reflexivity.
+    + reflexivity.
+Qed.
+
+(* Proofs within Proofs *)
